d3dd36232595492191389e83321857ff098b6aac
Graham Bouvier <hamnixster@gmail.com>
2020-12-08 02:48:22 -0800

    fix bugs in day 8

--- 8.rb
+++ 8.rb
@@ -1,49 +1,72 @@
-part2_input = File.read("8").split("\n").map { |ins| ins.split.tap { |ins| ins[-1] = ins.last.to_i } }
-part1_input = Marshal.load(Marshal.dump(part2_input))
+Instruction = Struct.new(:id, :code, :value, :visited_at) do
+  def visited?
+    !!visited_at
+  end
 
-def run_intcode(instructions)
+  def jmp?
+    code == "jmp"
+  end
+
+  def nop?
+    code == "nop"
+  end
+end
+
+input = File.read("8").split("\n").map.with_index do |instr, id|
+  instr = instr.split
+  Instruction.new(id, instr[0], instr[1].to_i, nil)
+end
+
+def run_intcode(prg)
   pointer, acc, count = 0, 0, 0
-  while (inc = 1) && (ins = instructions[pointer]) && (val = ins[1]) && ins[-1] != "visited"
-    ins << count
+  while (instr = prg[pointer]) && !instr.visited?
+    instr.visited_at = count
     count += 1
-    ins << "visited"
-    case ins.first
+    case instr.code
     when "acc"
-      acc += val
+      acc += instr.value
+      pointer += 1
     when "jmp"
-      inc = val
+      pointer += instr.value
+    else
+      pointer += 1
     end
-    pointer += inc
   end
   acc
 end
 
-def swap_index(input, start = nil)
-  start ||= input.size - 1
-  bottom, top = start - 1, start
-  return start if input[start][-1] == "visited"
-  range_to_reach = loop {
-    bottom_ins = input[bottom]
-    return bottom if bottom_ins[-1] == "visited"
-    if (val = bottom_ins[1]) &&
-        bottom_ins[0] == "jmp" &&
-        (val <= 0 || val > (top - bottom) + 1)
-      break [bottom + 1, top]
+def find_swap_instr(prg, final_id: prg.last.id)
+  candidates = []
+
+  loop_id = final_id - 1
+  upstream_range = loop {
+    instr = prg[loop_id]
+    if instr.visited?
+      candidates << instr
+      break [instr.id + 1, final_id]
     end
-    bottom -= 1
+    if instr.jmp? && (instr.value <= 0 || instr.value > (final_id - instr.id + 1))
+      break [instr.id + 1, final_id]
+    end
+    loop_id -= 1
   }
 
-  input.select.with_index { |ins, pos|
-    code, val = ins[..2]
-    (val + pos).between?(*range_to_reach) &&
-      ((code == "jmp" && val != 1) || (code == "nop" && ins[-1] == "visited"))
-  }.map { |candidate|
-    swap_index(input, input.index(candidate))
-  }.flatten.compact.min { |ins| ins[2] }
+  candidates += prg.select { |instr|
+    (instr.value + instr.id).between?(*upstream_range) &&
+      !instr.id.between?(*upstream_range) &&
+      ((instr.nop? && instr.visited?) || instr.jmp?)
+  }
+
+  candidates.map.with_index { |candidate, i|
+    if candidate.jmp? && !candidate.visited?
+      find_swap_instr(prg, final_id: candidate.id)
+    else
+      candidate
+    end
+  }.flatten.compact.uniq { |instr| instr.id }[0]
 end
 
-puts run_intcode(part1_input)
-
-swap_index(part1_input).tap { |index| part2_input[index][0] = part2_input[index][0] == "jmp" ? "nop" : "jmp" }
-
-puts run_intcode(part2_input)
+puts run_intcode(input)
+find_swap_instr(input).tap { |instr| instr.code = instr.jmp? ? "nop" : "jmp" }
+input.each { |instr| instr.visited_at = nil }
+puts run_intcode(input)
